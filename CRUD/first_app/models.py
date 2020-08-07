744f48a82e37358c3b69a2453e47e7501345e51d
isrt09 <mazedur@gmail.com>
2020-08-07 10:34:53 +0600

    Model & Migrations initialized

--- CRUD/first_app/models.py
+++ CRUD/first_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+	first_name  = models.CharField(max_length=50)
+	last_name	= models.CharField(max_length=50)
+	email 		= models.EmailField()
+	phone		= models.IntegerField()
+
+	def __str__(self):
+		return self.first_name+" "+self.last_name
+	
+	class Meta:
+		verbose_name       ='Stuent'    
+		verbose_name_plural='Stuents'    
+
+    
+
+
+
+
